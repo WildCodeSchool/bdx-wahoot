258783750f2c7fb210a1d86f355920c72b7df159
navyrandom <vdnhavy@gmail.com>
2021-04-13 16:00:10 +0200

    logo dans header, 2 boutons admin joueur dans User

---
+++ wahoot/src/View/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import User from './Components/User.jsx';
+
+
+const Home=() => {
+  
+    return (
+    <div>
+      <User/>
+    </div>
+    );
+    
+};
+export default Home;
