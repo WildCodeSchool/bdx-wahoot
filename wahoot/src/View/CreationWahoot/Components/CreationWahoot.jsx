8d1a56e0fed0647c7d01ec65091ff4311517d432
Esther241 <esther.dinh@gmail.com>
2021-04-13 16:19:47 +0200

    creation bouton save + bouton cancel

---
+++ wahoot/src/View/CreationWahoot/Components/CreationWahoot.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ButtonCancel from "./ButtonCancel";
+import ButtonSave from "./Buttonsave";
+
+
+
+const CreationWahoot = () => {
+
+    return (
+        <div>
+        <div> 
+          <ButtonSave/>
+        </div>
+       
+        <div>
+        <ButtonCancel/>  
+        </div>
+        </div>
+    );
+}
+
+
+export default CreationWahoot;
---
+++ wahoot/src/View/CreationWahoot/Components/QuestionsW.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import App from "./App";
+
+
+function questionsW () {
+    return (
+        <div>
+        <div>
+            <h2>Questions du Wahoot</h2>
+        </div>
+
+        <div>
+            <p>Pas de question pour le moment.</p>
+        </div>
+        </div>
+    );
+}
+
+export default questionsW;
