c4e1d3b9cb7844ef4eb533bc9497d0c2d88cdb6f
Esther241 <esther.dinh@gmail.com>
2021-04-29 16:25:17 +0200

    titre question

--- wahoot/src/View/StartGame/Components/QuestionsPlayer.jsx
+++ wahoot/src/View/StartGame/Components/QuestionsPlayer.jsx
@@ -1,33 +1,29 @@
+import { Card, CardContent, Typography } from "@material-ui/core";
+import React from "react";
 
-import { Card, CardContent, Typography } from '@material-ui/core';
-import React from 'react';
-import TitlePlayer from './TitlePlayer';
-
-const questionReps = [{
-    id:"123",
-    question : "Quel est le langage web ? ",
-}
+const questionReps = [
+  {
+    id: "123",
+    question: "Quel est le langage web ? ",
+  },
 ];
 
 const QuestionsPlayer = () => {
-    return (
-        <div>
-            {questionReps.map((qr) => {
-                return (
-                    <div>
-                        <Card>
-            <CardContent>
-            <Typography variant="h6">{qr.question}</Typography> 
-            </CardContent>
+  return (
+    <div>
+      {questionReps.map((qr) => {
+        return (
+          <div>
+            <Card>
+              <CardContent>
+                <Typography variant="h6">{qr.question}</Typography>
+              </CardContent>
             </Card>
-                    </div>
-                )
-            })}
-        </div>
-    )
+          </div>
+        );
+      })}
+    </div>
+  );
 };
-        
-           
-        
 
 export default QuestionsPlayer;
